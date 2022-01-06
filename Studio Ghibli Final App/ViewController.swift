3a0ef241a9bd3d98cdb128784bb95e47741bcf80
Lubabah-mujallid <90623112+Lubabah-mujallid@users.noreply.github.com>
2022-01-06 18:28:23 +0000

    Initial Commit

---
+++ Studio Ghibli Final App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studio Ghibli Final App
+//
+//  Created by administrator on 06/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
